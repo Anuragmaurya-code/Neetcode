7b7d4ea86b7e38942630a79a3402358534b0e892
Anurag Maurya <mauryaanurag375@gmail.com>
2023-06-11 15:05:28 +0530

    11 june

---
+++ Two pointer/Palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        newS=""
+        print(ord("9"))
+        for c in s:
+            if 97<=ord(c)<=122 or 48<=ord(c)<=57:
+                newS+=c
+        length=len(newS)
+        for i in range(length):
+            if newS[i]!=newS[length-1-i]:
+                return False
+        return True
+            
+
+x= Solution()
+print(x.isPalindrome("A man, a plan, a canal: Panama"))
